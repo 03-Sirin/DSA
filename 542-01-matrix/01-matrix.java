32ccd6cd2bd14b27b5fec333941eef6866cac4ea
sirin <sirinvifakgaa@gmail.com>
2026-08-03 09:21:40 +0530

    Time: 13 ms (90.07%) | Memory: 49.5 MB (28.75%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -2,15 +2,15 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int r=mat.length;
         int c=mat[0].length;
-        int[][] ans=new int[r][c];
-        boolean[][] visited=new boolean[r][c];
+        int[][] ans=new int[r][c];  
         Queue<int[]> q=new LinkedList<>();
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(mat[i][j] == 0){
                     ans[i][j]=0;
-                    visited[i][j]=true;
                     q.add(new int[]{i,j,0});
+                }else{
+                    ans[i][j]=-1;
                 }
             }
         }
@@ -22,9 +22,8 @@ class Solution {
                 int nr=curr[0]+rd[i];
                 int nc=curr[1]+cd[i];
                 int dist=curr[2];
-                if(nr>=0 && nc>=0 && nr<r && nc<c && !visited[nr][nc]){
+                if(nr>=0 && nc>=0 && nr<r && nc<c && ans[nr][nc] == -1){
                     ans[nr][nc]=dist+1;
-                    visited[nr][nc]=true;
                     q.add(new int[]{nr,nc,dist+1});
                 }
             }
